ce47c2d3fa97aaa33eda042fadd114dd44f752d4
xujin1999 <825414468@qq.com>
2023-07-03 22:44:51 +0800

    first update

--- src/main/java/com/example/mptest/Tq.java
+++ src/main/java/com/example/mptest/Tq.java
@@ -9,6 +9,7 @@ public class Tq {
     public static void main(String[] args) {
         int[] heights = {6,5,1};
         System.out.println(largestRectangleArea1(heights));
+        System.out.println(largestRectangleArea1(heights));
 
 //        System.out.println(removeKdigits("112", 1));
 
